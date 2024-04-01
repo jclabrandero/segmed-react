7ecb00b5dd6cda08baf25840257e515d9c71d1b6
Juan Carlos <jcharlylc@gmail.com>
2024-04-01 12:08:44 -0400

    Not allowed component

--- src/modules/basic/index.ts
+++ src/modules/basic/index.ts
@@ -1,9 +1,11 @@
 
 import { Home } from './home/home.view'
+import { NotAllowed } from './not-allowed/not-allowed.view'
 import { NotFound } from './not-found/not-found.view'
 
 
 export {
 	Home,
+	NotAllowed,
 	NotFound
 }
---
+++ src/modules/basic/not-allowed/not-allowed.view.tsx
@@ -0,0 +1,14 @@
+
+export function NotAllowed() {
+	return (
+		<div>
+			<div>
+
+			</div>
+			<div>
+				<h3>Acceso denegado</h3>
+				<p>¡Lo sentimos!, No tienes permisos para ver ésta página.</p>
+			</div>
+		</div>
+	)
+}
