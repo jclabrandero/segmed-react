bfb921990fc0f5ef5beee06da7bb37cecf957393
Juan Carlos <jcharlylc@gmail.com>
2024-04-24 22:30:51 -0400

    Provider type and export List

--- src/modules/reference/index.ts
+++ src/modules/reference/index.ts
@@ -1,8 +1,10 @@
 
 import { BelongingList } from './belonging/belonging-list.view'
 import { MedicalOfficeList } from './medical-office/medical-office-list.view'
+import { ProviderList } from './provider/provider-list.view'
 
 export {
 	BelongingList,
-	MedicalOfficeList
+	MedicalOfficeList,
+	ProviderList
 }
--- src/types/reference.type.ts
+++ src/types/reference.type.ts
@@ -1,4 +1,7 @@
 
+import { MedicalGroup } from '.'
+
+
 export type Belonging = {
 	id:		number
 	name:	string
@@ -13,4 +16,18 @@ export type MedicalOffice = {
 	status:		number
 
 	belonging:	Belonging
+}
+
+export type Provider = {
+	id:				number
+	vendorCode:		string
+	businessName:	string
+	nit?:			string
+	address?:		string
+	phone?:			string
+
+	status:			number
+
+	belonging:		Belonging
+	medicalGroups:	Array<MedicalGroup>
 }
