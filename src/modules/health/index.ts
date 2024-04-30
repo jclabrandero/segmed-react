42ea9de9cb06e0afa8b3880caaf79a171dcf83c3
Juan Carlos <jcharlylc@gmail.com>
2024-04-30 11:05:07 -0400

    Clinic care index

---
+++ src/modules/health/index.ts
@@ -0,0 +1,8 @@
+
+import { ClinicCareList } from './clinic-care/clinic-care-list.view'
+import { ClinicCareManage } from './clinic-care/clinic-care-manage.view'
+
+export {
+	ClinicCareList,
+	ClinicCareManage
+}
