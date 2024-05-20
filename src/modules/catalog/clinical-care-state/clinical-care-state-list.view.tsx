878873575e237756624b263eef190d001a65f6df
Juan Carlos <jcharlylc@gmail.com>
2024-05-20 14:00:45 -0400

    Permissions in clinic care state

--- src/modules/catalog/clinical-care-state/clinical-care-state-list.view.tsx
+++ src/modules/catalog/clinical-care-state/clinical-care-state-list.view.tsx
@@ -2,21 +2,24 @@
 import { useQuery, useSubscription } from '@apollo/client'
 import { Input, Space, Table, Tag } from 'antd'
 
-import { ErrorDialog, Loader, ToolBar, ToolBarMenu } from '../../../components'
-import { useError, useAntdHelp, useFilter } from '../../../hooks'
+import { ErrorDialog, ToolBar, ToolBarMenu } from '../../../components'
+import { useError, useAntdHelp, useFilter, useAuth } from '../../../hooks'
+import { NotAllowed } from '../../basic'
 
 import { query, subscription } from './clinical-care-state.constant'
 
 
 export function ClinicalCareStateList() {
 	const { addKey, tableStatus } = useAntdHelp()
+		, { has } = useAuth()
 		, [ error, onError ] = useError()
 		, { loading, data, refetch } = useQuery(query.CLINICAL_CARE_STATES, { onError })
-		, [ clinicalCareStates, filter ] = useFilter(addKey(data?.clinicalCareStates), ['name'])
+		, [ clinicalCareStates, filter ] = useFilter(addKey(data?.clinicalCareStates), ['name', 'description'])
 	const { Column } = Table
+
 	useSubscription(subscription.CLINICAL_CARE_STATE_UPSERTED, { onData: () => refetch() })
 
-	return (
+	return has('ReadClinicalCareState',
 		<>
 			<ToolBar>
 				<ToolBarMenu>
@@ -27,23 +30,26 @@ export function ClinicalCareStateList() {
 			<Table
 				size='middle'
 				dataSource={clinicalCareStates}
-				bordered={true}>
+				bordered={true}
+				scroll={{ x: true }}
+				loading={loading}
+			>
 				<Column title='Id' dataIndex='id'/>
-				<Column title='Nombre' dataIndex='name'/>
+				<Column title='Nombre' dataIndex='name' ellipsis/>
+				<Column title='Descripción' dataIndex='description' ellipsis/>
 				<Column title='Color' render={({ color }) => (
 					<Tag color={ color }>{ color }</Tag>
 				)}/>
-				<Column title='Bloqueo' render={({ lock }) => lock ? <b>Sí</b> : 'No'}/>
-				<Column title='Descripción' dataIndex='description'/>
+				<Column title='Bloqueo de consulta' ellipsis render={({ lock }) => lock ? <b>Sí</b> : 'No'}/>
 				<Column title='Estado' render={tableStatus}/>
-				<Column title='Acciones' width='6rem' render={() => (
+				<Column title='Acciones' width='6rem' fixed='right' render={() => has('WriteClinicalCareState',
 					<Space>
 					</Space>
 				)}/>
 			</Table>
 
-			<Loader show={loading}/>
 			<ErrorDialog error={error}/>
-		</>
+		</>,
+		<NotAllowed/>
 	)
 }
