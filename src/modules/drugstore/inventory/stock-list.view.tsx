40c95fa6f0cb9e401b05609b2649d5b3478fcdd7
Juan Carlos Labrandero <jcharlylc@gmail.com>
2024-12-27 10:44:33 -0400

    Medication stock list

---
+++ src/modules/drugstore/inventory/stock-list.view.tsx
@@ -0,0 +1,55 @@
+
+import { useQuery } from '@apollo/client'
+import { Table, Input } from 'antd'
+
+import { ErrorDialog, ToolBar, ToolBarMenu } from '../../../components'
+import { useError, useFilter, useAntdHelp } from '../../../hooks'
+import { Pharmacy } from '../../../types'
+
+import { query } from './inventory.constant'
+
+export function StockList({ pharmacyId }: { pharmacyId: number }) {
+	const { addKey } = useAntdHelp()
+	const [ error, onError ] = useError()
+		, { loading, data } = useQuery(query.INVENTORIES, { onError, variables: { pharmacyId } })
+		, [ inventories, filter ] = useFilter(addKey<Pharmacy>(data?.inventories), ['stock'])
+	const { Column } = Table
+
+	return (
+		<>
+			<ToolBar>
+				<ToolBarMenu>
+					<Input.Search enterButton allowClear onSearch={filter}/>
+				</ToolBarMenu>
+				<ToolBarMenu>
+					<></>
+				</ToolBarMenu>
+			</ToolBar>
+			<Table
+				size='middle'
+				pagination={false}
+				bordered={true}
+				scroll={{ x: true }}
+				dataSource={inventories}
+				loading={loading}
+			>
+				<Column title='Código' render={({ medication }) => (
+					<span>{ medication.code }</span>
+				)}/>
+				<Column title='Nombre' ellipsis render={({ medication }) => (
+					<span>{ medication.name }</span>
+				)}/>
+				<Column title='Concentración' ellipsis render={({ medication }) => (
+					<span>{ medication.concentration }</span>
+				)}/>
+				<Column title='Unidad' ellipsis render={({ medication }) => (
+					<span>{ medication.unit.name }</span>
+				)}/>
+				<Column title='Stock' dataIndex='stock'/>
+				<Column title='Mínimo' dataIndex='min'/>
+			</Table>
+
+			<ErrorDialog error={error}/>
+		</>
+	)
+}
