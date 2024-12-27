ac397bfca3e9a2b4bb33c9122a57e81eaaa7d228
Juan Carlos Labrandero <jcharlylc@gmail.com>
2024-12-27 10:43:47 -0400

    Departure module

---
+++ src/modules/drugstore/inventory/departure-list.view.tsx
@@ -0,0 +1,96 @@
+
+import { useQuery, useSubscription } from '@apollo/client'
+import { Table, Input, Space } from 'antd'
+
+import { ErrorDialog, ToolBar, ToolBarMenu } from '../../../components'
+import { useError, useFilter, useAntdHelp, useAuth, useDate } from '../../../hooks'
+import { Departure, DepartureItem } from '../../../types'
+
+import { query, subscription } from './inventory.constant'
+import { CreateDeparture, CreateDepartureItem } from './departure-upsert.view'
+
+function DepartureItemList({ departureId }: { departureId: number }) {
+	const { addKey } = useAntdHelp()
+		, { format } = useDate()
+	const [ error, onError ] = useError()
+		, { loading, data } = useQuery(query.DEPARTURE_ITEMS, { onError, variables: { departureId } })
+	const { Column } = Table
+
+	return (
+		<>
+			<Table
+				size='small'
+				dataSource={addKey<DepartureItem>(data?.departureItems)}
+				bordered={ true }
+				loading={loading}
+				pagination={{ hideOnSinglePage: true }}>
+				<Column title='Lote' ellipsis render={({ batch }) => batch.code}/>
+				<Column title='Medicamento' ellipsis render={({ batch }) => {
+					const { code, name, concentration, unit} = batch.medication
+					return `${code} - ${name} - ${concentration} - ${unit.name}`
+				}}/>
+				<Column title='Fecha de expiración' ellipsis render={({ batch }) => format(batch.expireAt, 'dd/MM/yyyy')}/>
+				<Column title='Cantidad' ellipsis dataIndex='quantity'/>
+				<Column title='Precio' ellipsis dataIndex='price'/>
+				
+			</Table>
+			<ErrorDialog error={error}/>
+		</>
+	)
+}
+
+export function DepartureList({ pharmacyId }: { pharmacyId: number }) {
+	const { addKey } = useAntdHelp()
+		, { has } = useAuth()
+		, { format } = useDate()
+	const [ error, onError ] = useError()
+		, { loading, data, refetch } = useQuery(query.DEPARTURES, { onError, variables: { pharmacyId } })
+		, [ departures, filter ] = useFilter(addKey<Departure>(data?.departures), ['remark'])
+	const { Column } = Table
+
+	useSubscription(subscription.DEPARTURE_UPSERTED, { onData: () => {
+		console.log('DEPARTURE_UPSERTED')
+		refetch() }})
+
+	return (
+		<>
+			<ToolBar>
+				<ToolBarMenu>
+					<Input.Search enterButton allowClear onSearch={filter}/>
+				</ToolBarMenu>
+				<ToolBarMenu>
+					{ has('WriteInventory', <CreateDeparture pharmacyId={pharmacyId}/>) }
+				</ToolBarMenu>
+			</ToolBar>
+			<Table
+				size='middle'
+				dataSource={departures}
+				bordered={ true }
+				pagination={{ pageSize: 15 }}
+				scroll={{ x: true }}
+				loading={loading}
+				expandable={{
+					expandedRowRender: (departure) => {
+						return (<>
+							<div></div>
+							<DepartureItemList departureId={departure.id}/>
+						</>)
+					},
+					rowExpandable: () => true
+				}}
+			>
+				<Column title='Id' dataIndex='id'/>
+				<Column title='Observación' dataIndex='remark' ellipsis/>
+				<Column title='Fecha de salida' ellipsis render={({ departureDate }) => (
+					<span>{format(departureDate, 'dd/MM/yyyy')}</span>
+				)}/>
+				<Column title='Acciones' width='6rem' fixed='right' render={({ id }) => (
+					<Space>
+						<CreateDepartureItem departureId={id} pharmacyId={pharmacyId}/>
+					</Space>
+				)}/>
+			</Table>
+			<ErrorDialog error={error}/>
+		</>
+	)
+}
---
+++ src/modules/drugstore/inventory/departure-upsert.view.tsx
@@ -0,0 +1,190 @@
+
+import { useState } from 'react'
+import { useQuery, useSubscription } from '@apollo/client'
+import { Button, Form, Input, InputNumber, Space, Select, DatePicker, Spin } from 'antd'
+
+import { CreateDialog } from '../../../components'
+import { Departure, Inventory, Batch } from '../../../types'
+import { useAntdHelp } from '../../../hooks'
+import { subscription as batchSubscription } from '../batch/batch.constant'
+
+import { mutation, query } from './inventory.constant'
+
+interface IDepartureCreateArgs {
+	remark:			string
+	departureDate:	Date
+	pharmacyId:		boolean
+}
+
+interface IDepartureDependencies {
+	pharmacyId?:	number
+	departure?:		Departure
+}
+
+type DepartureFormProps = {
+	mode: 'create' | 'update'
+	data: IDepartureDependencies
+	onSubmit: (data: IDepartureCreateArgs) => void
+	onCancel: () => void
+}
+
+function DepartureForm({ mode, data, onSubmit, onCancel }: DepartureFormProps) {
+	const { departure, pharmacyId } = data
+		, { Item } = Form
+		, [ form ] = Form.useForm()
+		, { touched } = useAntdHelp()
+	const onFinish = () => {
+		const payload = touched(form)
+		if (mode == 'create') onSubmit({ pharmacyId, ...payload })
+	}
+
+	return (
+		<Form form={form} layout='vertical' autoComplete='off' onFinish={onFinish} initialValues={departure}>
+			<Item
+				name='remark'
+				label='Observación'
+				rules={[{ required: true, message: 'Escriba la observación' }]}>
+				<Input/>
+			</Item>
+			<Item
+				name='departureDate'
+				label='Fecha de salida'
+				rules={[{ required: true, message: 'Seleccione fecha de salida' }]}>
+				<DatePicker format='DD/MM/YYYY'/>
+			</Item>
+			<div className='modal-dialog-footer'>
+				<Space>
+					<Button type='default' onClick={onCancel}>Cancelar</Button>
+					<Button type='primary' htmlType='submit'>Aceptar</Button>
+				</Space>
+			</div>
+		</Form>
+	)
+}
+
+export function CreateDeparture({ pharmacyId }: { pharmacyId: number }) {
+	return (
+		<CreateDialog<IDepartureCreateArgs, IDepartureDependencies>
+			title='Nueva salida'
+			mutation={mutation.CREATE_DEPARTURE}
+			render={(submit, close) => <DepartureForm mode='create' data={{ pharmacyId }} onSubmit={submit} onCancel={close}/>}
+		/>
+	)
+}
+
+interface IDepartureItemCreateArgs {
+	quantity:		number
+	price:			number
+	batchId:		number
+	departureId:	number
+}
+
+interface IDepartureItemDependencies {
+	inventories:	Array<Inventory>
+}
+
+type DepartureItemFormProps = {
+	mode: 'create' | 'update'
+	data: IDepartureItemDependencies
+	onSubmit: (data: IDepartureItemCreateArgs) => void
+	onCancel: () => void
+	onRefetch: () => void
+	pharmacyId:		number
+	departureId:	number
+}
+
+function MedicationBatch({ pharmacyId, medicationId }: { pharmacyId: number; medicationId: number }) {
+	const { Item } = Form
+		, { map } = useAntdHelp()
+		, variables = { data: { pharmacyId, medicationId } }
+		, { loading, data } = useQuery(query.BATCHES_STOCK, { variables, fetchPolicy: 'network-only' })
+
+	if (loading) return <Spin/>
+
+	return (
+		<Item name='batchId' label='Lote' rules={[{ required: true, message: 'Seleccione el lote' }]}>
+			<Select
+				options={map(data?.batchesStocks, ({ batch, stock }: { batch: Batch, stock: number }) => {
+					return {
+						label: `${batch.code} - (stock: ${stock})`,
+						value: batch.id
+					}
+				})}
+				showSearch={true}
+				placeholder='Número de lote'
+			/>
+		</Item>
+	)
+}
+
+function DepartureItemForm({ mode, data, onSubmit, onCancel, onRefetch, pharmacyId, departureId }: DepartureItemFormProps) {
+	const { inventories } = data
+		, { Item } = Form
+		, [ form ] = Form.useForm()
+		, { touched, map } = useAntdHelp()
+		, [ medicationId, setMedicationId ] = useState(0)
+	const onFinish = () => {
+		const payload = touched(form)
+		if (mode == 'create') onSubmit({ departureId, ...payload })
+	}
+
+	useSubscription(batchSubscription.BATCH_UPSERTED, { onData: onRefetch })
+
+	return (
+		<Form form={form} layout='vertical' autoComplete='off' onFinish={onFinish}>
+			<Item label='Medicamento'>
+				<Select
+					options={map(inventories, (inventory: Inventory) => {
+						const { code, name, concentration, unit} = inventory.medication
+						return {
+							label: `${code} - ${name} - ${concentration} - ${unit.name}`,
+							value: inventory.medication.id
+						}
+					})}
+					showSearch={true}
+					placeholder='Medicamento'
+					onChange={value => {
+						form.setFieldValue('batchId', null)
+						setMedicationId(value)}
+					}
+				/>
+			</Item>
+			{
+				medicationId != 0 &&
+				<MedicationBatch pharmacyId={pharmacyId} medicationId={medicationId}/>
+			}
+			<Item
+				name='quantity'
+				label='Cantidad'
+				rules={[{ required: true, message: 'Escriba la cantidad' }]}>
+				<InputNumber min='1'/>
+			</Item>
+			<Item
+				name='price'
+				label='Precio'
+				rules={[{ required: true, message: 'Escriba el precio' }]}>
+				<InputNumber
+					min='0'
+					step='0.01'/>
+			</Item>
+			<div className='modal-dialog-footer'>
+				<Space>
+					<Button type='default' onClick={onCancel}>Cancelar</Button>
+					<Button type='primary' htmlType='submit'>Aceptar</Button>
+				</Space>
+			</div>
+		</Form>
+	)
+}
+
+export function CreateDepartureItem({ departureId, pharmacyId }: { departureId: number, pharmacyId: number }) {
+	return (
+		<CreateDialog<IDepartureItemCreateArgs, IDepartureItemDependencies>
+			title='Crear ítem'
+			query={query.INVENTORIES}
+			options={{ variables: { pharmacyId } }}
+			mutation={mutation.CREATE_DEPARTURE_ITEM}
+			render={(submit, close, data, refetch) => <DepartureItemForm mode='create' data={data} onSubmit={submit} onCancel={close} onRefetch={refetch} pharmacyId={pharmacyId} departureId={departureId}/>}
+		/>
+	)
+}
