33d9a51e749bce693249d159dd9dc0ccae3e076a
Juan Carlos Labrandero <jcharlylc@gmail.com>
2025-04-12 11:04:23 -0400

    feat: updated custumizable delete dialog

--- src/components/dialog/delete-dialog.tsx
+++ src/components/dialog/delete-dialog.tsx
@@ -1,5 +1,5 @@
 
-import { useEffect, useState } from 'react'
+import { useEffect, useState, ReactNode } from 'react'
 import { DocumentNode, useLazyQuery, useMutation } from '@apollo/client'
 import { Button, Modal } from 'antd'
 import { DeleteFilled } from '@ant-design/icons'
@@ -11,18 +11,22 @@ import { useError } from '../../hooks'
 type DeleteDialogProps<TDeleteArgs> = {
 	id:			number
 	title:		string
-	render:		(record: TDeleteArgs) => React.ReactNode
+	icon?:			ReactNode
+	render?:		(record: TDeleteArgs) => React.ReactNode
+	renderExt?: (record: TDeleteArgs) => React.ReactNode
 	query:		DocumentNode
 	mutation:	DocumentNode
 	removeData?:		object
+	confirmButtonText?: string
 }
 
-export function DeleteDialog<T>({ id, title, render, query, mutation, removeData }: DeleteDialogProps<T>) {
+export function DeleteDialog<T>({ id, title, icon, render, renderExt, query, mutation, removeData, confirmButtonText }: DeleteDialogProps<T>) {
 	const [ open, setOpen ] = useState(false)
 		, [ error, onError ] = useError()
 	const close = () => setOpen(false)
 	const [ get, { loading, data }] = useLazyQuery(query, { onError, fetchPolicy: 'no-cache' })
 		, [ remove, { loading: deleting } ] = useMutation(mutation, { onCompleted: close, onError })
+	const FinalIcon = icon || <DeleteFilled/>
 
 	useEffect(() => data && setOpen(true), [data])
 
@@ -33,7 +37,7 @@ export function DeleteDialog<T>({ id, title, render, query, mutation, removeData
 				type='text'
 				size='small'
 				className='table-toolbtn-danger'
-				icon={<DeleteFilled/>}
+				icon={FinalIcon}
 				onClick={() => get({ variables: { id } })}/>
 			<Modal
 				title={title}
@@ -44,11 +48,18 @@ export function DeleteDialog<T>({ id, title, render, query, mutation, removeData
 				footer={() => (
 					<>
 						<Button type='default' onClick={close}>Cancelar</Button>
-						<Button type='primary' danger onClick={() => removeData ? remove({ variables: { id, data: removeData }}) : remove({ variables: { id }})}>Eliminar</Button>
+						<Button type='primary' danger onClick={() => removeData ? remove({ variables: { id, data: removeData }}) : remove({ variables: { id }})}>
+							{ confirmButtonText || 'Eliminar' }
+						</Button>
 					</>
 				)}
 			>
-				<p>Confirme para eliminar {data && render(data)}</p>
+				{
+					render && <p>Confirme para eliminar {data && render(data)}</p>
+				}
+				{
+					renderExt && <p>{data && renderExt(data)}</p>
+				}
 			</Modal>
 			<Loader show={loading || deleting}/>
 			<ErrorDialog error={error}/>
